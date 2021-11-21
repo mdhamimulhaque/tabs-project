3b31fb79f5c789f0009f13a9dae3f9a91dab319a
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2021-11-21 17:51:16 +0600

    tabs project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import { useState,useEffect } from 'react';
 import './App.css';
+import { FaAngleDoubleRight } from "react-icons/fa";
+import Data from "./Data";
 
 function App() {
+
+  const [ jobs,setJobs ] = useState(Data);
+  const [ value,setValue ] = useState(0);
+ 
+  const { dates,company,duties,title,i } = jobs[value];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h2>Experience</h2>
+        <hr />
       </header>
-    </div>
+      <main>
+        <section className="btn-box">
+          {
+            jobs.map((com,index)=>{
+              return(
+                <button 
+                onClick={()=>setValue(index)}
+                className={`btn ${value === index && "active"} `}
+                >
+                  {com.company}
+                </button>
+              )
+            })
+            }
+        </section>
+        <section className="info-box">
+          <h4>{title}</h4>
+          <h5>{company}</h5>
+          <p>{dates}</p>
+          <article>
+            {
+              duties.map(( duty,index ) =>{
+                return(
+                  <div className="duty-text" key={index}>
+                    <FaAngleDoubleRight style={{color:"#F26457"}}/>
+                    <p>{duty}</p>
+                  </div>
+                )
+              })
+            }
+          </article>
+        </section>
+      </main>
+    </>
   );
 }
 
